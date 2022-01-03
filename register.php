5155ba2a8ff262a77f5d66fd07f3dd1214f4eb91
Aya66 <89361822+Aya66@users.noreply.github.com>
2022-01-03 22:50:42 +0200

    Add files via upload

---
+++ register.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+</head>
+<body>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "carrentalsystem";
+
+$fname = $_POST["fname"];   //gets input from user from customer_registeration.php
+$lname = $_POST["lname"];
+$email = $_POST["email"];
+$db = $_POST["db"];
+$gender = $_POST["gender"];
+$country = $_POST["country"];
+$pass = $_POST["pass"];
+
+$conn = new mysqli($servername, $username, $password, $dbname);  //creates the connection
+if ($conn->connect_error) {    						//checks the connection
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT customer_id, first_name, last_name, password, email, birthdate, country FROM customer WHERE email='$email'"; //checks if email already exists in db
+$result = $conn->query($sql);
+if ($result->num_rows > 0){
+	header('Location: /Car_rental_system/customer_registeration.php? error=1'); //sends error and closes connection
+	$conn->close();
+}
+
+$sql = "INSERT INTO customer (fname, lname, email, db, gender, country, password) VALUES ('$fname', '$lname', '$email', '$db', '$gender', '$country', '$pass')";  //puts the given data into the db
+
+if ($conn->query($sql) === TRUE) {  //if data is placed in db correctly, sends user to the login page
+  header('Location: /Car_rental_system/customer_login.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+</body>
+</html>
