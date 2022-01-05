b9cb3a8ecde31e8d1c916786d44f8d0b0de68a30
Moustafa-H <moustafa.hesham1999@gmail.com>
2022-01-05 12:41:53 +0200

    added random cities to users

--- DML.sql
+++ DML.sql
@@ -21,24 +21,24 @@ INSERT INTO `car` (`plate_id`, `model`, `body`, `brand`, `color`, `year`, `statu
 (8459, '500x', 'suv', 'fiat', 'blue', 2014, 'active', 5),
 (8755, 'a3', 'sedan', 'audi', 'white', 2017, 'active', 5);
 
-INSERT INTO `user` (`user_id`, `first_name`, `last_name`, `password`, `email`, `birthdate`, `gender`, `country`, `is_admin`) VALUES
-(1, 'Paki', 'Cairo', 'm617p6', 'p.cairo@hotmail.com', '1961-08-21', "", 'Canada', FALSE),
-(2, 'Amery', 'Hammett', '2fnf9e', 'a.hammett4143@hotmail.com', '1974-08-03', 'F', 'Chile', FALSE),
-(3, 'Stuart', 'Walker', 'c0hg2m', 'walker-stuart@outlook.com', '1961-04-17', 'M', 'United Kingdom', FALSE),
-(4, 'Stone', 'Tanner', 'p40dlb', 'tannerstone@yahoo.com', '1962-01-17', 'M', 'Netherlands', FALSE),
-(5, 'Lee', 'Simon', '17dn92', 's_lee@yahoo.com', '1976-08-10', 'F', 'China', FALSE),
-(6, 'Rowan', 'Merrill', 'wcmt6b', 'merrill_rowan1862@outlook.com', '1971-05-07', 'F', 'Ireland', FALSE),
-(7, 'Shaeleigh', 'Caleb', 'x5aab2', 'caleb-shaeleigh@hotmail.com', '1981-07-09', 'M', 'India', FALSE),
-(8, 'Isabelle', 'Forrest', 'qq8sta', 'iforrest@gmail.com', '1980-11-18', 'F', 'Nigeria', FALSE),
-(9, 'Arthur', 'Silas', 'y56cfw', 'a_silas957@outlook.com', '1974-08-03', 'M', 'Italy', FALSE),
-(10, 'Alana', 'Wade', 'cdnc5m', 'wadealana@gmail.com', '1979-07-14', 'F', 'New Zealand', FALSE),
-(11, 'Blaze', 'Alec', '6fm9y2', 'ablaze9300@outlook.com', '1998-01-15', 'M', 'Peru', FALSE),
-(12, 'Avye', 'Barclay', 'qaqs1k', 'barclayavye6981@yahoo.com', '1988-05-07', "", 'Colombia', FALSE),
-(13, 'Fiona', 'Jarrod', '8vdmq8', 'jfiona3989@gmail.com', '1996-04-21', 'F', 'Austria', FALSE),
-(14, 'Adena', 'Baxter', '69tu4t', 'adena.baxter@outlook.com', '1981-05-12', 'F', 'Ireland', FALSE),
-(15, 'Norman', 'Alec', 'lr2i97', 'alec.norman2131@yahoo.com', '1966-03-02', 'M', 'Pakistan', FALSE),
-(16, 'Jackson', 'Oleg', 'xsu986', 'joleg4886@hotmail.com', '1973-05-17', 'M', 'Vietnam', FALSE),
-(17, 'admin', 'admin', 'dv3o77', 'carrentalsystem@gmail.com', '2000-01-01', "", 'Egypt', TRUE);
+INSERT INTO `user` (`user_id`, `first_name`, `last_name`, `password`, `email`, `birthdate`, `gender`, `country`, `city`, `is_admin`) VALUES
+(1, 'Paki', 'Cairo', 'm617p6', 'p.cairo@hotmail.com', '1961-08-21', "", 'Canada', 'Mundare', FALSE),
+(2, 'Amery', 'Hammett', '2fnf9e', 'a.hammett4143@hotmail.com', '1974-08-03', 'F', 'Chile', 'Florida', FALSE),
+(3, 'Stuart', 'Walker', 'c0hg2m', 'walker-stuart@outlook.com', '1961-04-17', 'M', 'United Kingdom', 'Watford', FALSE),
+(4, 'Stone', 'Tanner', 'p40dlb', 'tannerstone@yahoo.com', '1962-01-17', 'M', 'Netherlands', 'Rotem', FALSE),
+(5, 'Lee', 'Simon', '17dn92', 's_lee@yahoo.com', '1976-08-10', 'F', 'China', 'Hong Kong', FALSE),
+(6, 'Rowan', 'Merrill', 'wcmt6b', 'merrill_rowan1862@outlook.com', '1971-05-07', 'F', 'Ireland', 'Belfast', FALSE),
+(7, 'Shaeleigh', 'Caleb', 'x5aab2', 'caleb-shaeleigh@hotmail.com', '1981-07-09', 'M', 'India', 'Mumbai', FALSE),
+(8, 'Isabelle', 'Forrest', 'qq8sta', 'iforrest@gmail.com', '1980-11-18', 'F', 'Nigeria', 'Lagos', FALSE),
+(9, 'Arthur', 'Silas', 'y56cfw', 'a_silas957@outlook.com', '1974-08-03', 'M', 'Italy', 'Empoli', FALSE),
+(10, 'Alana', 'Wade', 'cdnc5m', 'wadealana@gmail.com', '1979-07-14', 'F', 'New Zealand', 'Levin', FALSE),
+(11, 'Blaze', 'Alec', '6fm9y2', 'ablaze9300@outlook.com', '1998-01-15', 'M', 'Peru', 'Iquitos', FALSE),
+(12, 'Avye', 'Barclay', 'qaqs1k', 'barclayavye6981@yahoo.com', '1988-05-07', "", 'Colombia', 'Saravena', FALSE),
+(13, 'Fiona', 'Jarrod', '8vdmq8', 'jfiona3989@gmail.com', '1996-04-21', 'F', 'Austria', 'Telfs', FALSE),
+(14, 'Adena', 'Baxter', '69tu4t', 'adena.baxter@outlook.com', '1981-05-12', 'F', 'Ireland', 'Dublin', FALSE),
+(15, 'Norman', 'Alec', 'lr2i97', 'alec.norman2131@yahoo.com', '1966-03-02', 'M', 'Pakistan', 'Kotli', FALSE),
+(16, 'Jackson', 'Oleg', 'xsu986', 'joleg4886@hotmail.com', '1973-05-17', 'M', 'Norway', 'Grimstad', FALSE),
+(17, 'admin', 'admin', 'dv3o77', 'carrentalsystem@gmail.com', '2000-01-01', "", 'Egypt', 'Alexandria', TRUE);
 
 INSERT INTO `reservation` (`reservation_id`, `user_id`, `plate_id`, `office_id`, `reservation_date`) VALUES
 (1, 4, 8459, 5, '2021-08-03');
