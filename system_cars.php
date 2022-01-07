139058a2c74b830878c9ca26206002593846e026
Moustafa-H <moustafa.hesham1999@gmail.com>
2022-01-07 07:47:15 +0200

    updated system_cars //save changes remaining

--- system_cars.php
+++ system_cars.php
@@ -34,38 +34,151 @@
 	<section class=" white-colour font20 scrollbar">
 
 		<?php
-	
-		$servername = "localhost";
-		$username = "root";
-		$password = "";
-		$dbname = "carrentalsystem";
 
-		$conn = new mysqli($servername, $username, $password, $dbname);  //creates the connection
-		if ($conn->connect_error) {        //checks the connection
-			die("Connection failed: " . $conn->connect_error);
+		if (isset($_POST["search"])) {
+			$searchedValModel = $_POST["searchedValModel"];
+			$searchedValColor = $_POST["searchedValColor"];
+			$query = "SELECT * FROM car WHERE (CONCAT(`plate_id`, `model`, `body`, `brand`, `color`, `year`, `status`, `office_id`) LIKE '%".$searchedValModel."%'
+			AND CONCAT(`plate_id`, `model`, `body`, `brand`, `color`, `year`, `status`, `office_id`) LIKE '%".$searchedValColor."%')";
+			$searchResults = getQueryResults($query);
 		}
-		$sql = "SELECT * 
-				FROM car";
-		$result = $conn->query($sql);  //gets the data of the user with the given inputs
+		else {
+			$query = "	SELECT *
+						FROM car
+						ORDER BY plate_id";
+			$searchResults = getQueryResults($query);
+		}
 
-		if ($result->num_rows > 0) {
-			echo "plate_id model body brand color year status office_id <br>";
-			while ($row = $result->fetch_assoc()) {  // check if the data exists
-				echo'	<div class="item-bar black-background">
-						<h4 class="item-margins">'.
-						$row["plate_id"]." ".$row["model"]." ".$row["body"]." ".$row["brand"]." ".$row["color"]." ".$row["year"]." ".$row["status"]." ".$row["office_id"]."<br>"
-						.'</h4>
-						</div>';
+		function getQueryResults($query) {
+			$servername = "localhost";
+			$username = "root";
+			$password = "";
+			$dbname = "carrentalsystem";
+			$conn = mysqli_connect($servername, $username, $password, $dbname);  //creates the connection
+			$filteredResult = mysqli_query($conn, $query);
+			return $filteredResult;
+		}
+
+		if (isset($_POST["save"])) {
+
+			$servername = "localhost";
+			$username = "root";
+			$password = "";
+			$dbname = "carrentalsystem";
+			// Create connection
+			$conn = new mysqli($servername, $username, $password, $dbname);
+			// Check connection
+			if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+			}
+
+			"UPDATE car
+			SET column1 = value1, column2 = value2, ...
+			WHERE condition";
+		
+		}
+
+		if (isset($_POST["add"])) {
+
+			$servername = "localhost";
+			$username = "root";
+			$password = "";
+			$dbname = "carrentalsystem";
+			// Create connection
+			$conn = new mysqli($servername, $username, $password, $dbname);
+			// Check connection
+			if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+			}
+
+			$newPlateID = $_POST["newPlateID"];
+
+			if (validatePlateID($newPlateID)) {
+				$sql = "INSERT INTO car (`plate_id`, `model`, `body`, `brand`, `color`, `year`, `status`, `office_id`) VALUES
+				($newPlateID, '', '', '', '', '', '', NULL)";
+			
+				if ($conn->query($sql) === TRUE) {
+					echo "New record created successfully";
+					header("location:system_cars.php");
+				}
+				else {
+					echo '	<h5 class="white-colour">
+							Error: ' . $sql . "<br>" . $conn->error . '
+							</h5>';
+				}
+			}
+			else {
+				echo '<h5 class="white-colour">"Error: Plate ID should be 4 digits long"</h5>' ;
+			}
+
+			$conn->close();
+
+		}
+
+		function validatePlateID($newPlateID) {
+			if (strlen($newPlateID) == 4) {
+				return 1;
+			}
+			else {
+				return 0;
 			}
 		}
-		else {  //if data does not exit, sends error to login page
-			echo "no cars exist";
-			//header('Location: /car-rental-system/user_login.php? error=1');
-		}
-		
-		$conn->close();
 
 		?>
+
+		<form action="system_cars.php" method="POST">
+
+			<br>
+			<input type="text" name="searchedValModel" placeholder="Search here">
+			<input type="text" name="searchedValColor" placeholder="Search here">
+			<input type="submit" name="search" value="Filter">
+			<input type="text" name="newPlateID" placeholder="New Car Plate ID">
+			<input type="submit" name="add" value="Add New Car">
+			<input type="submit" name="save" value="Save Changes">
+			<br>
+
+			<br>
+			<table class="white-colour font20">
+
+				<tr>
+					<th>plate number</th>
+					<th>model</th>
+					<th>body</th>
+					<th>brand</th>
+					<th>color</th>
+					<th>year</th>
+					<th>status</th>
+					<th>office</th>
+				</tr>
+				<?php while($row = mysqli_fetch_array($searchResults)):?>
+				<tr>
+					<td><input type="text" name="plateID" value=<?php echo $row["plate_id"];?> readonly></td>
+					<td><input type="text" name="carModel" value=<?php echo $row["model"];?>></td>
+					<td><input type="text" name="carBody" value=<?php echo $row["body"];?>></td>
+					<td><input type="text" name="carBrand" value=<?php echo $row["brand"];?>></td>
+					<td><input type="text" name="carColor" value=<?php echo $row["color"];?>></td>
+					<td><input type="number" name="carYear" min="1900" max="2099" step="1" value=<?php echo $row["year"];?>></td>
+					<td>
+						<?php
+						echo '<select name="carStatus">';
+						if ($row["status"] == "out_of_service") {
+							echo '	<option value="out_of_service">Out of Service</option>
+									<option value="active">Active</option>';
+						}
+						else {
+							echo '	<option value="active">Active</option>
+									<option value="out_of_service">Out of Service</option>';
+						}
+						echo '</select>';
+						?>
+					</td>
+					<td><input type="text" name="officeID" value=<?php echo $row["office_id"];?>></td>
+				</tr>
+				<?php endwhile;?>
+			
+			</table>
+		
+		</form>
 
     </section>
 
